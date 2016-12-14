35ee64c3b4c5633d8e841e0ed9f913891242c6c1
payne-chris-r <06cpayne@gmail.com>
2016-12-14 10:08:45 -0500

    Add ability to assign folder name and add extension
    Add the ability to prepend a folder name to the key. That folder name
    is currently today's date. Also adds the extension which required
    the installation of mime. Mime is added as a dependency to package.json
    in order to use it.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -7,23 +7,34 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
+const mime = require('mime');
+const path = require('path');
+
 let file = {
   path: process.argv[2],
   title: process.argv[3]
 };
 
-console.log("file is ", file);
+let contentType = mime.lookup(file.path);
+let ext = path.extname(file.path);
+
+let folder = new Date().toISOString().split('T')[0];
+
+console.log("folder is ", folder);
+
+//  NOTHING WILL EVER GO IN STANLEY'S FOLDER.
+console.log("contentType is ", contentType);
 
 let stream = fs.createReadStream(file.path);
 let bucket = process.env.AWS_S3_BUCKET_NAME;
 
-console.log("bucket is ", bucket);
-
 const params = {
   ACL: 'public-read',
   Bucket: bucket,
-  Key: file.title,
+  Key: `${folder}/${file.title}${ext}`,
+  // Key: folder + '/' + file.title + ext,
   Body: stream,
+  ContentType: contentType,
 };
 
 s3.upload(params, function(error,data){
