448eb274e1fe65348345b6a0584b7bf9bdbe4ede
payne-chris-r <06cpayne@gmail.com>
2016-12-15 09:24:40 -0500

    Use bin/aws file and Upload model to save to mongo
    Implemented mongoose and added an Upload model to my bin/aws file
    so that after the s3 upload is successful, I store the correct
    information in my mongoDB. Information that I'm storing is title
    and url (aws location).

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -3,15 +3,31 @@
 require('dotenv').load();
 
 const s3Upload = require('../lib/aws-s3-upload');
+const mongoose = require('../app/middleware/mongoose');
+const Upload = require('../app/models/upload');
 
 let file = {
   path: process.argv[2],
   title: process.argv[3]
 };
 
+// Upload.create(upload?)
+
 s3Upload(file)
-  .then(function(data){
+  .then(function(response){
     console.log("inside then block");
-    console.log("data is ", data);
+    console.log("response is ", response);
+    console.log("url is ", response.Location);
+    return Upload.create({
+      url: response.Location,
+      title: file.title,
+    });
   })
-  .catch(console.error);
+  .then(function(upload){
+    console.log("mongo create was successful");
+    console.log("upload is ", upload);
+  })
+  .catch(console.error)
+  .then(function(){
+    mongoose.connection.close();
+  });
